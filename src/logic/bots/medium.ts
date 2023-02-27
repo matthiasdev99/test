bfddd87cca3a0168b1fe1556704e0775a795105d
Tany06 <115782073+Tany06@users.noreply.github.com>
2023-02-27 11:08:25 +0100

    pettymove

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -12,7 +12,7 @@ export function mediumMove(board: Field[], own: Field): number {
   for (const move of blanks) {
     const copyBoard = [...board];
     copyBoard[move] = own;
-    moves.set(move, minimax(copyBoard, invertPlayer(own), own, false));
+    moves.set(move, pettyMove(copyBoard, own));
     copyBoard[move] = Field.EMPTY;
   }
 
@@ -23,45 +23,20 @@ export function mediumMove(board: Field[], own: Field): number {
 
   return bestScore[0];
 
-  return -1;
 }
 
-// the minimax algorithm is used to determine the outcome (between -1 and 1) for a given board
-function minimax(
-  board: Field[],
-  currentPlayer: Field,
-  own: Field,
-  maximize: boolean
-): number {
+
+
+// this bot:
+// - blocks the player from winning, if it can
+// - chooses a random move otherwise
+export function pettyMove(board: Field[], own: Field): number {
   const blanks = getBlanks(board);
 
   const winner = won(board);
   if (winner === own) return 1; // win
   else if (winner === invertPlayer(own)) return -1; // loss
   else if (!blanks.length) return 0; // draw
-
-  let bestScore: number = maximize ? -Infinity : Infinity;
-
-  for (const move of blanks) {
-    board[move] = currentPlayer;
-    if (maximize) {
-      const score = minimax(board, invertPlayer(currentPlayer), own, false);
-      bestScore = Math.max(bestScore, score);
-    } else {
-      const score = minimax(board, invertPlayer(currentPlayer), own, true);
-      bestScore = Math.min(bestScore, score);
-    }
-    board[move] = Field.EMPTY;
-  }
-
-  return bestScore;
-}
-
-// this bot:
-// - blocks the player from winning, if it can
-// - chooses a random move otherwise
-export function pettyMove(board: Field[], own: Field): number {
-
 
   return -1 
 }
